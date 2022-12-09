d7bfca1bc2474341066b37127f4bbc0b8cd243e7
cpanato <ctadeu@gmail.com>
2022-12-09 15:47:02 +0100

    bump kind to use v0.17.0 and kubectl v1.25.5
    Signed-off-by: cpanato <ctadeu@gmail.com>

--- kind.sh
+++ kind.sh
@@ -18,9 +18,9 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-DEFAULT_KIND_VERSION=v0.16.0
+DEFAULT_KIND_VERSION=v0.17.0
 DEFAULT_CLUSTER_NAME=chart-testing
-DEFAULT_KUBECTL_VERSION=v1.23.12
+DEFAULT_KUBECTL_VERSION=v1.25.5
 
 show_help() {
 cat << EOF
