884e2250cf384c560b4f9e959017c74712491c25
cpanato <ctadeu@gmail.com>
2024-02-13 15:17:38 +0100

    update kind to release v0.21.0
    Signed-off-by: cpanato <ctadeu@gmail.com>

--- kind.sh
+++ kind.sh
@@ -18,9 +18,9 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-DEFAULT_KIND_VERSION=v0.20.0
+DEFAULT_KIND_VERSION=v0.21.0
 DEFAULT_CLUSTER_NAME=chart-testing
-DEFAULT_KUBECTL_VERSION=v1.26.6
+DEFAULT_KUBECTL_VERSION=v1.28.6
 
 show_help() {
 cat << EOF
